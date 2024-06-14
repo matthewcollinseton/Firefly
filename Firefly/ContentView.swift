bd788c702f4e792096b81689d9cb7a9845b8860e
Matthew Collins <m.collins@etoncollege.org.uk>
2024-06-14 10:00:54 +0100

    Initial Commit

---
+++ Firefly/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Firefly
+//
+//  Created by Collins, Matthew - MC on 14/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Firefly/FireflyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireflyApp.swift
+//  Firefly
+//
+//  Created by Collins, Matthew - MC on 14/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct FireflyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
